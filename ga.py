0a6d82c5e74397f34e50d020c82dbd1eabb66375
Jakub Sobolewski <jakupsob@gmail.com>
2018-12-18 22:51:29 +0100

    Added mutation

--- ga.py
+++ ga.py
@@ -25,19 +25,20 @@ for img in glob.glob("{:s}//{:s}".format(args.output,"*.png")):
     os.remove(img)
 
 image = cv.imread("{:s}//{:s}".format(args.input,"input_resized.png"),cv.IMREAD_UNCHANGED)
-
+cv.imwrite("{:s}/input.png".format(args.output), image)   
+    
 extracted = []
 for img in glob.glob("{:s}//{:s}".format(args.input,"c_*.png")):
     extracted.append(cv.imread(img,cv.IMREAD_UNCHANGED))
 
 (H, W) = image.shape[:2]
 print('Image shape = [{:d},{:d}]'.format(H,W))
-print("Score: identity={:.2f}, empty={:.2f}".format(ssim(image,image,multichannel=True),ssim(image,np.zeros(image.shape,dtype=np.uint8),multichannel=True)))
+print("SSIM score: identity={:.2f}, empty={:.2f}".format(ssim(image,image,multichannel=True),ssim(image,np.zeros(image.shape,dtype=np.uint8),multichannel=True)))
 
 def bitfield(n):
-    return [bool(digit) for digit in bin(n)[2:]]
+    return [bool(int(digit)) for digit in bin(n)[2:]]
 
-def int_to_bitfield(integer,integer_max):
+def int_to_bitfield(integer,integer_max=8):
     if integer < integer_max:
         n = int(np.log2(integer_max) + 1)
         integer_bits = bitfield(integer)
@@ -90,9 +91,8 @@ class Chunk:
         layer[y1min:y1max, x1min:x1max] += self.img[y2min:y2max, x2min:x2max]
         return layer   
 
-
 def draw(images):
-    output = np.zeros(image.shape, dtype=np.uint8)
+    output = np.full(image.shape,255, dtype=np.uint8)
     for im in images:
         # im.rotate()
         # im.resize()
@@ -127,38 +127,44 @@ def new_generation(parents, size, number_of_best):
             ind2 = np.random.randint(low=0,high=len_imgs)
         img1 = parents[ind1]
         img2 = parents[ind2]
-
-        for extra1,extra2 in zip(img1,img2):
+        for extra1,extra2 in zip(img1,img2):      
             pos_1y, pos_1x = int_to_bitfield(extra1.pos[0],H), int_to_bitfield(extra1.pos[1],W)
             pos_2y, pos_2x = int_to_bitfield(extra2.pos[0],H), int_to_bitfield(extra2.pos[1],W)
-
+           
             cross = np.random.randint(low=1,high=len(pos_1y)-1)
             pos_chy = pos_1y[:cross] + pos_2y[cross:]
             
             cross = np.random.randint(low=1,high=len(pos_1x)-1)
             pos_chx = pos_1x[:cross] + pos_2x[cross:]
-            
+           
             mutation_ps = np.random.random(len(pos_chx)+len(pos_chy))
             for i,(x,y) in enumerate(zip(pos_chx,pos_chy)):
                 if mutation_ps[i] < mutation_p:
-                    pos_chy[i] = not y 
+                    pos_chy[i] = not y
                 if mutation_ps[i+len(pos_chy)] < mutation_p:
                     pos_chx[i] = not x
-                
+                  
+            angle_1 = int_to_bitfield(int(extra1.angle))
+            angle_2 = int_to_bitfield(int(extra2.angle))
+            cross = np.random.randint(low=1,high=len(angle_1)-1)
+            angle_ch = angle_1[:cross] + angle_2[cross:]
+            mutation_ps = np.random.random(len(angle_ch))
+            for i,a in enumerate(angle_ch):
+                if mutation_ps[i] < mutation_p:
+                    angle_ch[i] = not a
+
+            scale_chy = np.abs(np.random.normal(loc=extra1.scale[0],scale=1))
+            scale_chx = np.abs(np.random.normal(loc=extra1.scale[1],scale=1))
             
-            scale_y = np.abs(np.random.normal(loc=extra1.scale[0],scale=1))
-            scale_x = np.abs(np.random.normal(loc=extra1.scale[1],scale=1))
-            angle = np.random.normal(loc=extra1.angle,scale=1)
-            
-            chunks.append(Chunk(extra1.img,[scale_y,scale_x],angle,[bitfield_to_int(pos_chy)%H,bitfield_to_int(pos_chx)%W]))
+            chunks.append(Chunk(extra1.img,[scale_chy,scale_chx],bitfield_to_int(angle_ch),[bitfield_to_int(pos_chy)%H,bitfield_to_int(pos_chx)%W]))
         generation.append(chunks)
-        if len(generation) == size - number_of_best:
+        if len(parents) + len(generation) == size - number_of_best:
             break
-    return generation
+    return parents + generation
 
 def fitness_func(score,i):
     # return some score function
-    return score
+    return score**((i+1)/100) + 10
 
 def fitness(generation, i):
     n_fitness = {}
@@ -169,11 +175,11 @@ def fitness(generation, i):
         fitness[g] = score
     return fitness, n_fitness
 
-generation_size = 10
+generation_size = 500
 number_of_best = 2
-generation = [[Chunk(img,[1,1],np.random.randint(low=-180,high=180),[np.random.randint(low=0,high=H),np.random.randint(low=0,high=W)]) for img in extracted] for i in range(generation_size)]
+generation = [[Chunk(img,[1,1],0,[np.random.randint(low=0,high=H),np.random.randint(low=0,high=W)]) for img in extracted] for i in range(generation_size)]
 
-num_iterations = 100
+num_iterations = 10
 iterations = np.arange(start=0,stop=num_iterations,step=1)
 best_score = []
 average_score = []
@@ -181,8 +187,7 @@ for i in iterations:
     print("i={:d}".format(i))
     (fit, n_fit) = fitness(generation,i)
     output = draw(generation[max(fit.iteritems(), key=operator.itemgetter(0))[0]])
-    if i % 10 == 0:
-        cv.imwrite("{:s}/out_{:d}.png".format(args.output,i), output)   
+    cv.imwrite("{:s}/out_{:d}.png".format(args.output,i), output)   
     best_score.append(max(fit.iteritems(), key=operator.itemgetter(1))[1])
     average_score.append(sum(fit.values())/(len(fit.values())))
     parents = [generation[weighted_random_choice(n_fit)] for i in range(int(generation_size/2) - number_of_best)]
@@ -193,4 +198,6 @@ plt.figure(num=None, figsize=(10, 5), dpi=80, facecolor='w', edgecolor='k')
 plt.plot(iterations,best_score,'-r',label="best")
 plt.plot(iterations,average_score,'-b',label="average")
 plt.legend()
+plt.xlabel("iteration")
+plt.ylabel("ssim")
 plt.savefig("{:s}/plot.png".format(args.output))
