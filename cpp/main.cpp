171cac4e0f0b8ddfb54e8c1deea95a2ed59f8939
Jakub Sobolewski <jakupsob@gmail.com>
2018-12-29 11:55:30 +0100

    Added image reconstruction

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,11 +5,12 @@
 #include <string>
 #include <dirent.h>
 #include <fstream>
+#include <assert.h> 
 
 using namespace cv;
 
 const char* INPUT_RESIZED = "input_resized.png";
-std::vector<Mat> img;
+std::vector<Mat> extracted;
 std::vector<int> params;
 Mat image;
 
@@ -20,24 +21,27 @@ int lattice_const;
 void CreateLattice()
 {
     lattice_const = params[0]/params[1];
-    lattice = new std::pair<int,int>[params[1]*params[1]];
-    for(int i = 0; i < params[1]; i++)
-        for(int j = 0; j < params[1]; j++)
-            lattice[i+j] = std::pair<int,int>(i*lattice_const,j*lattice_const);
+    int size = params[1];
+    lattice = new std::pair<int,int>[size*size];
+    for(size_t i = 0; i < size; i++)
+        for(size_t j = 0; j < size; j++)
+            lattice[i*size+j] = std::pair<int,int>(i*lattice_const,j*lattice_const);
+        
 }
 
 int Load(int argc, char **argv)
 {
     char buffer[50];
     sprintf(buffer, "%s/%s", argv[1], INPUT_RESIZED);
-    image = imread(buffer, CV_LOAD_IMAGE_COLOR);
+    image = imread(buffer, -1);
 
     if(!image.data)
     {
         std::cout <<  "Could not open or find the image" << "\n";
         return -1;
     }
-
+    imshow("image",image);
+    waitKey(0);
     DIR *dir;
     struct dirent *ent;
     printf("Reading from %s\n", argv[1]);
@@ -47,17 +51,17 @@ int Load(int argc, char **argv)
         {
             printf("%s\n", ent->d_name);
             sprintf(buffer, "%s/%s", argv[1], ent->d_name);
-            image = imread(buffer, CV_LOAD_IMAGE_COLOR);
-            if(image.data && std::string(ent->d_name).compare(std::string(INPUT_RESIZED)) != 0)
+            Mat img = imread(buffer, -1);
+            if(img.data && std::string(ent->d_name).compare(std::string(INPUT_RESIZED)) != 0)
             {
-                img.push_back(image);
+                extracted.push_back(img);
             }
         }
         closedir(dir);
     } 
     else 
     {
-        perror("error");
+        perror("LoadImages: Empty directory");
         return EXIT_FAILURE;
     }
     return 0;
@@ -79,8 +83,15 @@ void ReadParams(char* params_file)
     }
 }
 
-void DrawImage(size, images)
+void DrawImage(std::vector<Mat> images)
 {
+    Mat output(image.rows, image.cols, image.type());
+    size_t len = extracted.size() * extracted.size();
+    for(int i = 0; i < len; i++)
+        if(output.type() == extracted[i].type() && extracted[i].rows <= output.rows and extracted[i].cols <= output.cols)
+            extracted[i].copyTo(output(Rect(lattice[i].first, lattice[i].second,lattice_const,lattice_const)));
+    imshow("img",output);
+    waitKey(0);
 
 }
 
@@ -94,6 +105,7 @@ int main(int argc, char** argv)
     Load(argc, argv);
     ReadParams(argv[3]);
     CreateLattice();
+    DrawImage(extracted);
 
     return 0;
 }
