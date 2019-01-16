4dae29c088784f2e680a15b080a2c2618262986d
Paweł <paffcio314@gmail.com>
2019-01-16 22:52:50 +0100

    mssim

--- cpp/geneticAlgorithm.h
+++ cpp/geneticAlgorithm.h
@@ -66,8 +66,10 @@ public:
 
 	void Fitness() 
 	{
-		for(auto it = begin(generation); it != end(generation); ++it)
-       	  	it->setFitness(MSE(it->getImage()));
+		for(auto it = begin(generation); it != end(generation); ++it){
+			it->setFitness(getMSSIM(it->getImage(), res->image));
+       	  	// it->setFitness(MSE(it->getImage()));
+		}
 	}
 
 	void NewGeneration(std::vector<Image> parents, const float pMutation = 0.05)
@@ -133,15 +135,13 @@ public:
 		std::vector<Image> parents(nSelect + nBest);
 		std::vector<float> n_fitness(generation.size());
 		for(uint i = 0; i < generation.size(); i++)
-			n_fitness[i] = FitnessFunc(1./generation[i].getFitness(),iter);
-
+			n_fitness[i] = FitnessFunc(generation[i].getFitness(),iter);
 		for(int i = 0; i < nSelect; ++i)
 			parents[i] = generation[WeightedRandomChoice(n_fitness)];
 
 		std::vector<size_t> bestIndexes = SortIndexes(n_fitness);
 		for(int i = 0; i < nBest; i++)
 			parents[nSelect + i] = generation[bestIndexes[i]];
-
 		best = parents[nSelect+nBest-1];
 
 		return parents;
@@ -185,6 +185,60 @@ private:
 	void (GeneticAlgorithm::*crossover)(Image&, Image&, const Image&, const Image&);
 	FILE *output;
 	Image best;
+
+	float getMSSIM( const cv::Mat& i1, const cv::Mat& i2)
+	{
+		 const double C1 = 6.5025, C2 = 58.5225;
+		 /***************************** INITS **********************************/
+		 int d= CV_32F;
+
+		 cv::Mat I1, I2;
+		 i1.convertTo(I1, d);           // cannot calculate on one byte large values
+		 i2.convertTo(I2, d);
+
+		 cv::Mat I2_2   = I2.mul(I2);        // I2^2
+		 cv::Mat I1_2   = I1.mul(I1);        // I1^2
+		 cv::Mat I1_I2  = I1.mul(I2);        // I1 * I2
+
+		 /***********************PRELIMINARY COMPUTING ******************************/
+
+		 cv::Mat mu1, mu2;   //
+		 cv::GaussianBlur(I1, mu1, cv::Size(11, 11), 1.5);
+		 cv::GaussianBlur(I2, mu2, cv::Size(11, 11), 1.5);
+
+		 cv::Mat mu1_2   =   mu1.mul(mu1);
+		 cv::Mat mu2_2   =   mu2.mul(mu2);
+		 cv::Mat mu1_mu2 =   mu1.mul(mu2);
+
+		 cv::Mat sigma1_2, sigma2_2, sigma12;
+
+		 GaussianBlur(I1_2, sigma1_2, cv::Size(11, 11), 1.5);
+		 sigma1_2 -= mu1_2;
+
+		 GaussianBlur(I2_2, sigma2_2, cv::Size(11, 11), 1.5);
+		 sigma2_2 -= mu2_2;
+
+		 GaussianBlur(I1_I2, sigma12, cv::Size(11, 11), 1.5);
+		 sigma12 -= mu1_mu2;
+
+		 ///////////////////////////////// FORMULA ////////////////////////////////
+		 cv::Mat t1, t2, t3;
+
+		 t1 = 2 * mu1_mu2 + C1;
+		 t2 = 2 * sigma12 + C2;
+		 t3 = t1.mul(t2);              // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
+
+		 t1 = mu1_2 + mu2_2 + C1;
+		 t2 = sigma1_2 + sigma2_2 + C2;
+		 t1 = t1.mul(t2);               // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
+
+		 cv::Mat ssim_map;
+		 cv::divide(t3, t1, ssim_map);      // ssim_map =  t3./t1;
+
+		 cv::Scalar mssim = cv::mean( ssim_map ); // mssim = average of ssim map
+		 // std::cout<<mssim[0]+mssim[1]+mssim[2]+mssim[3];
+		 return mssim[0]+mssim[1]+mssim[2]+mssim[3];
+	}
 	
 	float MSE(const cv::Mat &imageToCompare)
 	{
